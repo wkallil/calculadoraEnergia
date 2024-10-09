f660bf778bdf7a1e0ea05c9a0b90748f8c5043b6
WKallil <kallil.tim@gmail.com>
2024-10-09 15:22:16 -0300

    atualização no SecurityConfig

--- src/main/java/com/interdisciplinar/calculadoraEnergia/Configs/SecurityConfig.java
+++ src/main/java/com/interdisciplinar/calculadoraEnergia/Configs/SecurityConfig.java
@@ -1,10 +1,13 @@
 package com.interdisciplinar.calculadoraEnergia.Configs;
 
+import com.google.cloud.storage.HttpMethod;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 @Configuration
@@ -18,14 +21,22 @@ public class SecurityConfig {
     }
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        return http
-                .csrf().disable()
-                .authorizeHttpRequests()
-                .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll() // Permitir acesso à documentação do Swagger
-                .anyRequest().authenticated()
-                .and()
-                .addFilter(new FirebaseAuthenticationFilter(firebaseTokenService))
-                .build();
+    public SecurityFilterChain filterChain(HttpSecurity http, FirebaseTokenService firebaseTokenService) throws Exception {
+
+        // Adiciona o filtro de autenticação do Firebase
+        FirebaseAuthenticationFilter firebaseAuthenticationFilter = new FirebaseAuthenticationFilter(firebaseTokenService);
+
+        http
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/swagger-ui.html", "/swagger-ui/**", "/v3/api-docs/**").permitAll()
+                        .anyRequest().authenticated())
+                .csrf(csrf -> csrf.disable())
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+
+                // Adiciona o filtro Firebase antes do UsernamePasswordAuthenticationFilter
+                .addFilterBefore(firebaseAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
     }
 }
