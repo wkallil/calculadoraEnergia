7f738ba2cb5d33e2631e832411f20a473e7b8f23
WKallil <kallil.tim@gmail.com>
2024-10-11 07:42:19 -0300

    atualizando a anotação do spring no FirebaseAuthenticationFilter

--- src/main/java/com/interdisciplinar/calculadoraEnergia/Configs/FirebaseAuthenticationFilter.java
+++ src/main/java/com/interdisciplinar/calculadoraEnergia/Configs/FirebaseAuthenticationFilter.java
@@ -7,11 +7,13 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 import java.util.Collections;
 
+@Component
 public class FirebaseAuthenticationFilter extends OncePerRequestFilter {
 
     private final FirebaseTokenService firebaseTokenService;
