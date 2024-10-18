59d1bd5f4befc14867507abfafb8f90145e5bf1f
WKallil <kallil.tim@gmail.com>
2024-10-18 12:23:56 -0300

    teste no BD2

--- src/main/java/com/interdisciplinar/calculadoraEnergia/controller/TesteBD.java
+++ src/main/java/com/interdisciplinar/calculadoraEnergia/controller/TesteBD.java
@@ -4,11 +4,15 @@ import com.interdisciplinar.calculadoraEnergia.model.Usuario;
 import com.interdisciplinar.calculadoraEnergia.repository.UsuarioRepository;
 import com.interdisciplinar.calculadoraEnergia.service.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+
+import static org.hibernate.query.sqm.tree.SqmNode.log;
 
 @RestController
 public class TesteBD {
@@ -16,9 +20,14 @@ public class TesteBD {
     @Autowired
     UsuarioRepository usuarioRepository;
 
-    @GetMapping("/teste-bd")
-    public ResponseEntity<List<Usuario>> testeBD() {
-        List<Usuario> result = usuarioRepository.findAll();
-        return ResponseEntity.ok(result);
+    @GetMapping("/usuarios")
+    public ResponseEntity<List<Usuario>> listarUsuarios() {
+        try {
+            List<Usuario> usuarios = usuarioRepository.findAll();
+            return ResponseEntity.ok(usuarios);
+        } catch (DataAccessException e) {
+            var message = e.getMessage();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
     }
 }
